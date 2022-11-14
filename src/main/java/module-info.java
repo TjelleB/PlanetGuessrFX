0bc8057b4bccdcce226875f81d52ae53c552b54c
letraeh <haertel.ivo@gmail.com>
2022-11-14 08:56:37 +0100

    Added javafx.graphics

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.planetguessrfx {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
 
     opens com.example.planetguessrfx to javafx.fxml;
