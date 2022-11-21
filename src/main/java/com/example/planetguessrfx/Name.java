5e10f517152dda60bf29045942c619e26f5c8c05
Instruction-Cache <78419216+Instruction-Cache@users.noreply.github.com>
2022-11-21 08:20:19 +0100

    Update Name.java

--- src/main/java/com/example/planetguessrfx/Name.java
+++ src/main/java/com/example/planetguessrfx/Name.java
@@ -2,7 +2,7 @@
 import java.util.Random;
 public class Name extends Generator {
   
-  private String[] names = {"Leon", "Kamela", "Marcel", "Hillary", "Edward", "Lisa", "Michael", "Marie", "Alexander", "Laura", "Felix", "Clara", "Lucas", "Charlotte", "Richard", "Elisabeth", "Julian", "Leonie", "Anton", "Emma", "Jeremy", "Johanna", "Joseph", "Leni", "Adam", "Anna",};
+  private String[] names = {"Leon", "Kamela", "Marcel", "Hillary", "Edward", "Lisa", "Michael", "Marie", "Alexander", "Laura", "Felix", "Clara", "Lucas", "Charlotte", "Richard", "Elisabeth", "Julian", "Leonie", "Anton", "Emma", "Jeremy", "Johanna", "Joseph", "Leni", "Adam", "Anna", "Bryan", "Lea", "Marc", "Olivia", "Jeff", "Sophia", "Ethan", "Emily", "Gregor", "Ellie", "Manuel", "Grace", "Henry", "Mia", "Sebastian", "Alina", "Jackson", "Isabella", "Jakob", "Ada", "Achim", "Karolin", "Alfred", "Ursula", "Bernd", "Valentina", "Norbert", "Vanessa", "Robert", "Theresa", "Thomas", "Gilberta", "Uwe", "Magret"};
   private String name;
   
   public Name()
