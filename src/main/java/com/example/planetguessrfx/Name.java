e1b0f296c40af25fb4c94d661a8f2012cd5c19b3
Tjelle Bonte <tjelle.bonte@gmail.com>
2022-11-29 09:06:03 +0100

    Namegen expanded

--- src/main/java/com/example/planetguessrfx/Name.java
+++ src/main/java/com/example/planetguessrfx/Name.java
@@ -1,19 +1,27 @@
 package com.example.planetguessrfx;
 import java.util.Random;
 public class Name extends Generator {
-  private final String[] greek_letters = {"Alpha", "Beta", "Gamma", "Delta"};
-  private final String[] numbers = {"I", "II", "III", "IV", "V"};
-  private String name;
+  private final String[] greek_letters = {"","", "", "Alpha", "Beta", "Gamma", "Delta"};
+  private final String[] numbers = {"","", "", "I", "II", "III", "IV", "V"};
+  private final String[] letter = {"", "", "", "a", "b", "c"};
+  private final String[] name = {"Spe", "Arion", "Arkas", "Orbitar", "Taphao Thong", "Taphao Kaew", "Dimidium", "Galileo", "Brahe", "Lipperhey", "Janssen", "Harriot", "Ægir"};
+  private String finalName;
   @Override
   public void generate() {
     Random rdm = new Random(); 
     int index = rdm.nextInt(greek_letters.length); 
-    name = (greek_letters[index]);
+    finalName = (greek_letters[index]) + " ";
+    index = rdm.nextInt(name.length);
+    finalName += (name[index]);
+    if(index >=3){finalName +=  " ";}
+    index = rdm.nextInt(letter.length);
+    finalName += (letter[index]);
+    if(index >=3){finalName +=  " ";}
     index = rdm.nextInt(numbers.length);
-    name +=  "-" + (numbers[index]);
+    finalName +=(numbers[index]);
   }
   public String getName() {
     this.generate();
-    return name;
+    return finalName;
   }
 }
