3f6d3b8f5361784fa30568e44843728ecdace371
letraeh <haertel.ivo@gmail.com>
2022-12-05 11:26:15 +0100

    Fixed main class

--- src/main/java/com/example/planetguessrfx/PlanetGuessr.java
+++ src/main/java/com/example/planetguessrfx/PlanetGuessr.java
@@ -1,28 +1,18 @@
 package com.example.planetguessrfx;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
-
-public class
-PlanetGuessr extends Application {
-
+public class PlanetGuessr extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("mainMenu.fxml"));
-        Parent root = loader.load();
-        Scene scene = new Scene(root, 480, 640);
         stage.setTitle("PlanetGuessr");
-        stage.setScene(scene);
+        stage.setScene(new Scene(loader.load(), 480, 640));
         stage.show();
     }
-
-
-
     public static void main(String[] args) {
         launch();
     }
