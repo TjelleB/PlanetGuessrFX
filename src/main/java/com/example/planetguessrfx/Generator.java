3646190a0610d2250b1c6bef7cc2c3e3a7cefcd3
letraeh <haertel.ivo@gmail.com>
2022-12-19 09:50:51 +0100

    redo ne shit

--- src/main/java/com/example/planetguessrfx/Generator.java
+++ src/main/java/com/example/planetguessrfx/Generator.java
@@ -2,29 +2,17 @@ package com.example.planetguessrfx;
 /*
 Ersteller: Frederick
 */
-
 public abstract class Generator {
-  
-  // start attributes
   private int planetID;
-  // end attributes
   
-  public Generator()
-  {
+  public Generator() {
     this.setPlanetID(planetID);
   }
 
-  
   // Abstrakte Methode, kein Algorithmus vorzugeben
   protected abstract void generate();
-  
-  
-  public int getPlanetID() {
-    return planetID;
-  }
 
   public void setPlanetID(int planetIDNew) {
     planetID = planetIDNew;
   }
-  
 }
